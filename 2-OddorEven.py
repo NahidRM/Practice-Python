1ce98376b05df7c74865ceabd78e4b3ee1ccc479
Nahid Muzammil <nahidrmz@gmail.com>
2017-02-07 12:05:50 +0800

    Update 2-OddorEven.py

--- 2-OddorEven.py
+++ 2-OddorEven.py
@@ -1,19 +1,13 @@
 a = int(input('Enter a number for God\'s sake '))
-
 if a%2 == 0:
     print('Yo, it\'s even..')
 else:
     print('It is odd like you')
-
-
 if a%4==0:
     print('Jackpot! It is a multiple of 4 too!')
-
-
 print('Now, prepare to enter two numbers')
 num = int(input('The first number: '))
 check = int(input('The second number: '))
-
 if num%check == 0:
     print(str(check)+' divides evenly into '+str(num))
 else:
