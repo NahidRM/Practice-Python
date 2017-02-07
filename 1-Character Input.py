b5c43eb3ac311550120788fd830fb26c2820c4b7
Nahid Muzammil <nahidrmz@gmail.com>
2017-02-07 12:05:13 +0800

    Update 1-Character Input.py

--- 1-Character Input.py
+++ 1-Character Input.py
@@ -1,10 +1,5 @@
 from datetime import date
 name = input('Enter your name: ')
 age = int(input('Enter your age: '))
-
 print('You will be a 100 years old in the year: ' + str(date.today().year + (100-age)))
-
-
 print('You will probably be dead by then anyway..')
-
-      
